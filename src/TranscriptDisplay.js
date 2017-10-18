b59dac0bfef39ab4bef75d1ae737d37274b1ba9a
Alex Norton <alexnorton@fastmail.com>
2017-10-18 19:51:59 +0100

    Convert TranscriptDisplay

--- src/TranscriptDisplay.js
+++ src/TranscriptDisplay.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Transcript } from 'transcript-model';
 
 import './TranscriptDisplay.css';
 
@@ -12,105 +11,97 @@ const SPAN_TYPES = {
 };
 
 const getTranscriptSpans = (transcript, matches) => {
-  const segments = transcript.segments.toArray().map((segment, segmentIndex) => {
-    let span = {
-      type: null,
-      words: [],
-    };
+  let span = {
+    type: null,
+    words: [],
+  };
 
-    const spans = [];
+  const spans = [];
 
-    for (let wordIndex = 0; wordIndex < segment.words.size; wordIndex += 1) {
-      let spanType;
+  for (let index = 0; index < transcript.length; index += 1) {
+    let spanType;
 
-      if (matches.filter(match =>
-        segmentIndex === match.start.segment
-        && wordIndex >= match.start.word
-        && wordIndex < match.start.word + match.start.length,
-      ).length) {
-        spanType = SPAN_TYPES.MATCH_START;
-      } else if (matches.filter(match =>
-        match.replacement
-        && segmentIndex === match.start.segment
-        && wordIndex >= match.start.word + match.start.length
-        && (
-          (!match.end && wordIndex < match.start.word + match.start.length + 1)
-          || (match.end && wordIndex < match.end.word)
-        ),
-      ).length) {
-        spanType = SPAN_TYPES.REPLACED;
-      } else if (matches.filter(match =>
-        match.end
-        && segmentIndex === match.end.segment
-        && wordIndex >= match.end.word
-        && wordIndex < match.end.word + match.end.length,
-      ).length) {
-        spanType = SPAN_TYPES.MATCH_END;
-      } else {
-        spanType = null;
-      }
-
-      if (spanType !== span.type) {
-        if (span.words.length > 0) {
-          spans.push(span);
-        }
-
-        if (span.type === SPAN_TYPES.REPLACED
-          && (spanType === null || spanType === SPAN_TYPES.MATCH_END)) {
-          const thisMatch = matches.filter(match =>
-            segmentIndex === match.start.segment
-            && wordIndex === match.start.word + match.start.length + 1,
-          )[0];
-
-          spans.push({
-            type: SPAN_TYPES.REPLACEMENT,
-            words: [
-              { text: thisMatch.replacement },
-            ],
-          });
-        }
-
-        span = {
-          type: spanType,
-          words: [],
-        };
-      }
-
-      const word = segment.words.get(wordIndex);
-
-      span.words.push({
-        start: word.start,
-        end: word.end,
-        text: word.text,
-      });
+    if (matches.filter(match =>
+      index >= match.start.index
+      && index < match.start.index + match.start.length,
+    ).length) {
+      spanType = SPAN_TYPES.MATCH_START;
+    } else if (matches.filter(match =>
+      match.replacement
+      && index >= match.start.index + match.start.length
+      && (
+        (!match.end && index < match.start.index + match.start.length + 1)
+        || (match.end && index < match.end.index)
+      ),
+    ).length) {
+      spanType = SPAN_TYPES.REPLACED;
+    } else if (matches.filter(match =>
+      match.end
+      && index >= match.end.index
+      && index < match.end.index + match.end.length,
+    ).length) {
+      spanType = SPAN_TYPES.MATCH_END;
+    } else {
+      spanType = null;
     }
 
-    spans.push(span);
+    if (spanType !== span.type) {
+      if (span.words.length > 0) {
+        spans.push(span);
+      }
 
-    return { spans };
-  });
+      if (span.type === SPAN_TYPES.REPLACED
+        && (spanType === null || spanType === SPAN_TYPES.MATCH_END)) {
+        const thisMatch = matches.filter(match =>
+          index === match.start.index + match.start.length + 1,
+        )[0];
 
-  return { segments };
+        spans.push({
+          type: SPAN_TYPES.REPLACEMENT,
+          words: [
+            { text: thisMatch.replacement },
+          ],
+        });
+      }
+
+      span = {
+        type: spanType,
+        words: [],
+      };
+    }
+
+    const word = transcript[index];
+
+    span.words.push({
+      start: word.start,
+      end: word.end,
+      text: word.text,
+    });
+  }
+
+  spans.push(span);
+
+  return spans;
 };
 
 const TranscriptDisplay = ({ transcript, matches }) => {
   const spans = getTranscriptSpans(transcript, matches);
 
-  return (<div>
-    {spans.segments.map((segment, segmentIndex) => (
-      <p key={segmentIndex}>
-        {segment.spans.map((span, spanIndex) => (
-          <span key={spanIndex} className={span.type && `span_${span.type}`}>
-            {span.words.map(word => word.text).join(' ')}
-          </span>
-        )).reduce((prev, curr) => [prev, ' ', curr])}
-      </p>
-    ))}
-  </div>);
+  return (
+    <p>
+      {spans.map((span, spanIndex) => (
+        <span key={spanIndex} className={span.type && `span_${span.type}`}>
+          {span.words.map(word => word.text).join(' ')}
+        </span>
+      )).reduce((prev, curr) => [prev, ' ', curr])}
+    </p>
+  );
 };
 
 TranscriptDisplay.propTypes = {
-  transcript: PropTypes.instanceOf(Transcript).isRequired,
+  transcript: PropTypes.arrayOf(
+    PropTypes.shape(),
+  ).isRequired,
   matches: PropTypes.arrayOf(
     PropTypes.shape(),
   ),
