6b9a44a49c8dc06dc54ebc3f8e4c8236f603ca64
Alex Norton <alexnorton@fastmail.com>
2017-10-22 19:23:11 +0100

    Rewrite correction matching

---
+++ src/__tests__/matchCorrection.test.js
@@ -0,0 +1,117 @@
+import matchCorrection, {
+  getTokens,
+  getForwardsMatches,
+  getBackwardsMatches,
+} from '../helpers/matchCorrection';
+
+const transcript = [
+  'hair.', 'Lots', 'of', 'it', 'splendid', 'teeth', 'and', 'find', 'your', 'lines', 'and', 'had', 'to', 'find',
+];
+
+describe('getTokens', () => {
+  it('converts input to tokens correctly', () => {
+    const input = 'Scottish Government remains committed strongly to the principle of giving Scotland, a choice at the end of this process.';
+
+    const output = getTokens(input);
+
+    expect(output).toEqual(['scottish', 'government', 'remains', 'committed', 'strongly', 'to', 'the', 'principle', 'of', 'giving', 'scotland', 'a', 'choice', 'at', 'the', 'end', 'of', 'this', 'process']);
+  });
+});
+
+describe('matchCorrection', () => {
+  it('returns no matches', () => {
+    const correction = 'blah hello strawberries';
+
+    const match = matchCorrection(transcript, correction);
+
+    expect(match).toEqual(null);
+  });
+
+  it('returns complete matches', () => {
+    const correction = 'and fine jaw lines';
+
+    const match = matchCorrection(transcript, correction);
+
+    expect(match).toEqual({
+      start: { index: 6, length: 1 },
+      replacement: 'fine jaw',
+      end: { index: 9, length: 1 },
+    });
+  });
+
+  it('returns partial matches', () => {
+    const correction = 'and fine jaw';
+
+    const match = matchCorrection(transcript, correction);
+
+    expect(match).toEqual({
+      start: { index: 6, length: 1 },
+      replacement: 'fine jaw',
+      end: null,
+    });
+  });
+});
+
+describe('getForwardsMatches', () => {
+  it('returns no matches', () => {
+    const tokens = ['strawberries', 'hello'];
+
+    const forwardsMatches = getForwardsMatches(transcript, tokens);
+
+    expect(forwardsMatches).toEqual([]);
+  });
+
+  it('returns multiple matches', () => {
+    const tokens = ['and', 'fine', 'jaw'];
+
+    const forwardsMatches = getForwardsMatches(transcript, tokens);
+
+    expect(forwardsMatches).toEqual([
+      { index: 6, length: 1 },
+      { index: 10, length: 1 },
+    ]);
+  });
+
+  it('it returns different length matches', () => {
+    const tokens = ['and', 'find'];
+
+    const forwardsMatches = getForwardsMatches(transcript, tokens);
+
+    expect(forwardsMatches).toEqual([
+      { index: 6, length: 2 },
+      { index: 10, length: 1 },
+    ]);
+  });
+});
+
+describe('getBackwardsMatches', () => {
+  it('returns no matches', () => {
+    const tokens = ['and', 'fine', 'jaw'];
+
+    const backwardsMatches = getBackwardsMatches(transcript, tokens);
+
+    expect(backwardsMatches).toEqual([]);
+  });
+
+  it('returns multiple matches', () => {
+    const tokens = ['something', 'whatever', 'and'];
+
+    const backwardsMatches = getBackwardsMatches(transcript, tokens);
+
+    expect(backwardsMatches).toEqual([
+      { index: 6, length: 1 },
+      { index: 10, length: 1 },
+    ]);
+  });
+
+  it('returns different length matches', () => {
+    const tokens = ['it', 'something', 'teeth', 'and'];
+
+    const backwardsMatches = getBackwardsMatches(transcript, tokens);
+
+    expect(backwardsMatches).toEqual([
+      { index: 5, length: 2 },
+      { index: 10, length: 1 },
+    ]);
+  });
+});
---
+++ src/helpers/matchCorrection.js
@@ -0,0 +1,149 @@
+const normaliseToken = input => (
+  input.toLowerCase().replace(/[^A-Za-z ]/g, '')
+);
+
+const getTokens = input => (
+  input
+    .trim()
+    .split(' ')
+    .map(normaliseToken)
+);
+
+const getForwardsMatches = (transcript, tokens) => {
+  const matchRoots = transcript
+    .map((word, index) => ({ word, index }))
+    .filter(({ word }) => word === tokens[0])
+    .map(({ index }) => ({ index, length: 1 }));
+
+  return matchRoots.map((initialMatch) => {
+    let finished = false;
+
+    const match = initialMatch;
+
+    for (let i = 1; !finished && i < tokens.length; i += 1) {
+      if (transcript[match.index + i] === tokens[i]) {
+        match.length += 1;
+      } else {
+        finished = true;
+      }
+    }
+
+    return match;
+  });
+};
+
+const getBackwardsMatches = (transcript, tokens) => {
+  const matchRoots = transcript
+    .map((word, index) => ({ word, index }))
+    .filter(({ word }) => word === tokens[tokens.length - 1])
+    .map(({ index }) => ({ index, length: 1 }));
+
+  return matchRoots.map((initialMatch) => {
+    let finished = false;
+
+    const match = initialMatch;
+
+    for (let i = 1; !finished && i < tokens.length; i += 1) {
+      if (transcript[match.index - i] === tokens[tokens.length - i - 1]) {
+        match.length += 1;
+      } else {
+        finished = true;
+      }
+    }
+
+    return {
+      length: match.length,
+      index: (match.index - match.length) + 1,
+    };
+  });
+};
+
+const matchCorrection = (transcript, correction) => {
+  const tokens = getTokens(correction);
+
+  // Get forwards matches
+  const forwardsMatches = getForwardsMatches(transcript, tokens);
+
+  // If there are forwards matches
+  if (forwardsMatches.length > 0) {
+    let start;
+    let end = null;
+    let replacement = null;
+
+    if (forwardsMatches.length === 1) {
+      start = forwardsMatches[0];
+    } else {
+      // Sort by length
+      const sortedForwardsMatches = forwardsMatches.sort(
+        (a, b) => b.length - a.length,
+      );
+
+      // Get longest forward matches
+      const longestForwardsMatches = sortedForwardsMatches.filter(
+        match => match.length === sortedForwardsMatches[0].length
+      );
+
+      // If there is more than one
+      if (longestForwardsMatches.length > 0) {
+        // Choose the first one
+        start = longestForwardsMatches
+          .sort((a, b) => a.index - b.index)[0];
+      } else {
+        // Otherwise we have our longest match
+        start = longestForwardsMatches[0];
+      }
+    }
+
+    // Get backwards matches
+    const backwardsMatches = getBackwardsMatches(transcript, tokens.slice(start.length));
+
+    // If there are backwards matches
+    if (backwardsMatches.length > 0) {
+      if (backwardsMatches.length === 1) {
+        end = backwardsMatches[0];
+      } else {
+        // Sort by length
+        const sortedBackwardsMatches = backwardsMatches.sort(
+          (a, b) => b.length - a.length,
+        );
+
+        // Get longest backwards matches
+        const longestBackwardsMatches = sortedBackwardsMatches.filter(
+          match => match.length === sortedBackwardsMatches[0].length
+        );
+
+        // If there is more than one
+        if (longestBackwardsMatches.length > 0) {
+          // Choose the first one
+          end = longestBackwardsMatches
+            .sort((a, b) => a.index - b.index)[0];
+        } else {
+          // Otherwise we have our longest match
+          end = longestBackwardsMatches[0];
+        }
+      }
+    }
+
+    if (start.length < tokens.length) {
+      replacement = tokens
+        .slice(start.length, end ? tokens.length - end.length : tokens.length)
+        .join(' ');
+    }
+
+    return {
+      start,
+      end,
+      replacement,
+    };
+  }
+
+  return null;
+};
+
+export default matchCorrection;
+
+export {
+  getTokens,
+  getForwardsMatches,
+  getBackwardsMatches,
+};
